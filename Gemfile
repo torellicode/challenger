3f49b82aadd9946804dc0c3b5aa46e568e9f00dd
torellicode <torellicode@gmail.com>
2024-11-19 02:38:44 -0500

    Add sidkiq gem and create worker in procfile.dev

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem "cssbundling-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Add these after your existing gems
+gem 'sidekiq'
+gem 'redis'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  require 'sidekiq'
+  require 'sidekiq/web'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +9,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Protect the Sidekiq admin dashboard with authentication
+  authenticate :user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+  
 end
